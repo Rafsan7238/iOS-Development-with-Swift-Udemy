48a89116e3d00f166a5a59ce32bdc5d5e50b02dd
Rafsan Al Mamun <rafsan7238@gmail.com>
2022-01-18 21:04:04 +0600

    Uploaded Section 3 Challenge Project Files

---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Rafsan Al Mamun on 18/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
