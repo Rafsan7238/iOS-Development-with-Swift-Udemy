218937432728528d8cfbfcccf9737f3e7ca4b7cf
Rafsan Al Mamun <rafsan7238@gmail.com>
2022-04-25 16:02:16 +0600

    Section 26: Built the Sentiment Classifier Model

---
+++ Twittermenti/Twittermenti/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Twittermenti
+//
+//  Created by Angela Yu on 17/07/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import UIKit
+import SwifteriOS
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var backgroundView: UIView!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var sentimentLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBAction func predictPressed(_ sender: Any) {
+    
+    
+    }
+    
+}
+
