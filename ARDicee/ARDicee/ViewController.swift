3dc2d043913a9c2a659790d8e9bc3417f4ec80ba
Rafsan Al Mamun <rafsan7238@gmail.com>
2022-04-29 15:23:14 +0600

    Section 27: Built a Basic Dice Scene

---
+++ ARDicee/ARDicee/ViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  ARDicee
+//
+//  Created by Rafsan Al Mamun on 29/4/22.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+//        let sphere = SCNSphere(radius: 0.2)
+//        let material = SCNMaterial()
+//
+//        material.diffuse.contents = UIColor.red
+//        sphere.materials = [material]
+//
+//        let node = SCNNode()
+//        node.position = SCNVector3(0, 0.1, -0.5)
+//
+//        node.geometry = sphere
+//
+//        sceneView.scene.rootNode.addChildNode(node)
+        sceneView.autoenablesDefaultLighting = true
+        
+        // Create a new scene
+        let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")!
+        
+        if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {
+            diceNode.position = SCNVector3(0, 0, -0.1)
+        
+            sceneView.scene.rootNode.addChildNode(diceNode)
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+
+}
